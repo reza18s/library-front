3dd281fbddefb15cb640c5b37346bd3889f05fc4
Majid Askarifarsangi <maf256@gmail.com>
2024-11-01 03:56:19 +0100

    addd selected in ModalBook

--- src/components/book/ModalBook.tsx
+++ src/components/book/ModalBook.tsx
@@ -1,9 +1,32 @@
 import { ModalDialog } from "@mui/joy";
-import { Button, DialogTitle, FormControl, FormLabel, Input, Modal, Stack, TextField } from "@mui/material";
-import React from "react";
+import { Button, DialogTitle, FormControl, FormLabel, Input, InputLabel, MenuItem, Modal, Select, Stack, TextField } from "@mui/material";
+import React, { useEffect } from "react";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function ModalBook() {
+
+  // const queryClient = useQueryClient();  // Correct way to access QueryClient
+  const { data:genre, error:genreError, isLoading:genreLoading } = useQuery({
+    queryKey: ["genres"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3000/genre");
+      if (!response.ok) throw new Error("Failed to fetch genres");
+      return response.json();
+    },
+  });
+  const { data:author, error:authorError, isLoading:authorLoading } = useQuery({
+    queryKey: ["authors"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3000/author");
+      if (!response.ok) throw new Error("Failed to fetch authors");
+      return response.json();
+    },
+  });
+  // if (genreLoading || authorLoading) return <div>Loading...</div>;
+  // if (genreError || authorError) return <div>Error occurred!</div>;
   const [open, setOpen] = React.useState<boolean>(false);
+  const [genreID, setGenreID] = React.useState<number>();
+  const [authorID, setAuthorID] = React.useState<number>();
   const [book, setBook] = React.useState({
     title: '',
     publication_year: '',
@@ -12,14 +35,31 @@ export default function ModalBook() {
     author_id: '',
     genre_id: ''
   })
-
-
+useEffect(() => {
+  console.log("genreLoading", genreLoading);
+  
+  if (!genreLoading && !authorLoading) {
+    setGenreID(genre[0].id);
+    setAuthorID(author[0].id);
+  }
+}, [genre, author]);
+  console.log(genre, author);
   const handleClose = () => {
     setOpen(false);
   };
-  const options = ['Option 1', 'Option 2', 'Option 3'];
-
-
+  // const options = ['Option 1', 'Option 2', 'Option 3'];
+  console.log("genreID", genreID);
+  console.log("authorID", authorID);
+  const genreItems = genre?.map((genre: { id: React.Key | readonly string[] | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+    <MenuItem key={genre.id} value={genre.id}>
+      {genre.name}  
+    </MenuItem>
+  ))
+  const authorItems = author?.map((author: { id: React.Key | readonly string[] | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+    <MenuItem key={author.id} value={author.id}>
+      {author.name}  
+    </MenuItem>
+  ))
     return (
         <React.Fragment>
         <Button
@@ -47,17 +87,30 @@ export default function ModalBook() {
                     value={book.title}
                     onChange={(e) => setBook({...book, title: e.target.value})} // Update state on input change
                     />
-                    <FormLabel>Genre</FormLabel>
-                    <TextField
-                        label="Choose an option"
-                        variant="outlined"
-                        inputProps={{ list: 'datalistOptions' }}
-                    />
-                    <datalist id="datalistOptions">
-                        {options.map((option, index) => (
-                        <option key={index} value={option} />
-                        ))}
-                    </datalist>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Genre</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={genreID}
+                  label="Genre"
+                  onChange={(event) => setGenreID(Number(event.target.value))}
+                  >
+                  {genreItems}
+                </Select>
+              </FormControl>                    
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">authorID</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={authorID}
+                  label="author"
+                  onChange={(event) => setAuthorID(Number(event.target.value))}
+                  >
+                  {authorItems}
+                </Select>
+              </FormControl>  
 
                   <FormLabel>copies_available</FormLabel>
                   <Input 
